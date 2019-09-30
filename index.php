17dd8830ea88d14b34e8121c5951aa92400fe10e
najlabathool <najla989j6@gmail.com>
2019-09-30 14:36:00 +0530

    Initial

---
+++ index.php
@@ -0,0 +1,167 @@
+<?php defined('_JEXEC') or die('Restricted access');?>
+<!DOCTYPE html>
+<html xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+<head>
+<jdoc:include type="head" />
+<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <script src="bootstrap/scripts/jquery.min.js"></script>
+      <script src="/bootstrap/js/bootstrap.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/style.css" type="text/css" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+
+
+<body>
+<div class="container" style="background-image:<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>
+/images/download1.jpeg"> 
+<div class="menubar">
+<ul style="font-family: 'Open Sans', sans-serif;font-weight:800">
+  <li ><a href="">Clean Kerala <span>Company Ltd</span></a></li>
+  <li style="float:right"><a href="">About</a></li>
+  <li style="float:right"><a href="">Programmers</a></li>
+  <li style="float:right"><a class="active1" href="#about">Contact</a></li>
+</ul><p style="font-family:'Open Sans', sans-serif;font-weight:800;padding-top:185px">
+<center><h1>
+STATE OF OUR SURROUNDINGS<br>
+IS THE CONDITION OF OUR SOUL</h1></center></p></div>
+
+
+
+
+</div>
+  
+<div class="secbar">
+    <ul style="list-style-type:none;padding:0px;margin-bottom:0px;margin-top:0px; background-color:#dcdee0;height:44px;font-family: 'Open Sans', sans-serif;
+font-weight:400" >
+<li><a style="display: block;float:left;
+  color: #093d17;width:682px;
+  text-align: left;
+  padding: 14px 16px;
+  padding-left:50px;
+  text-decoration:none;" href="">Clean Kerala<span>Company Ltd</span></a></li>
+<li class="rightclass"style="float:left;"><a style="display: block;
+  color:white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;" href="" class="active">Home</a></li>
+<li class="rightclass"><a  style="display: block;
+  color:#093d17;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;" href="">About us</a></li>
+  <li class="rightclass"><a  style="display: block;
+  color:#093d17;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;" href="">Programmes</a></li>
+  <li class="rightclass"><a  style="display: block;
+  color:#093d17;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;" href="">Projects</a></li>
+  <li class="rightclass"><a style="display: block;
+  color:#093d17;
+  text-align:center;
+  padding: 14px 16px;
+  text-decoration:none" href="">Progress</a></li>
+  <li class="rightclass"><a  style="display: block;
+  color: #093d17;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;" href="">Media</a></li>
+  <li class="rightclass"><a  style="display: block;
+  color:#093d17;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration:none;" href="">Contact us</a></li>
+  </ul>
+</div>
+<div class="vd">
+  <center style="font-family: 'Open Sans', sans-serif;
+font-weight:800">  LOREM IPSUM DOLOR SIT AMET</center> 
+<center style="font-family: 'Open Sans', sans-serif;
+font-weight:400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus mi sit amet libero elementum porta. Aliquam sit
+<br>amet ultricies mi, ac faucibus velit. Morbi aliquet massa quam, ac hendrerit metus fermentum sed. Duis tempus dignissim</br>
+enim eu tempor. Aliquam elementum libero lectus, in condimentum diam venenatis nec. Donec eget dignissim elit.<br>
+Curabitur pulvinar felis a vulputate pretium. Sed id fringilla mi. Proin vel massa eros.</center>
+<center  style="padding-top: 25px;">
+<iframe width="620" height="245" src="https://www.youtube.com/embed/tgbNymZ7vqY"></center>
+</iframe>
+</div>
+<div class="impact" >
+  <center style="font-family: 'Open Sans', sans-serif;
+font-weight:800">OUR IMPACT</center></div>
+<div class="circlediv">
+
+<div class="circle" style="float:left;">1235</div>
+
+<div class="circle"style="float:left;margin-left:25px">135</div>
+<div class="circle"style="float:left;margin-left:25px">637</div>
+<div class="circle"style="float:left;margin-left:25px">827</div>
+  
+</div>
+<div class="circletext" style="padding-top:130px;padding-left:50px;font-family: 'Open Sans', sans-serif;
+font-weight:300">
+  Kg of waste desposed.
+<span style="padding-left:58px">collection units</span>
+<span style="padding-left:68px">Local Bodies</span>
+<span style="padding-left:52px">Tones of waste recycled</span>
+
+
+
+
+</div>
+<div class="circlediv">
+
+<div class="circle" style="float:left;">1235</div>
+
+<div class="circle"style="float:left;margin-left:25px">135</div>
+<div class="circle"style="float:left;margin-left:25px">637</div>
+<div class="circle"style="float:left;margin-left:25px">827</div>
+  
+</div>
+<div class="circletext" style="padding-top:130px;padding-left:0px;font-family: 'Open Sans', sans-serif;
+font-weight:300">
+  Kg of waste desposed.
+<span style="padding-left:58px">collection units</span>
+<span style="padding-left:68px">Local Bodies</span>
+<span style="padding-left:52px">Tones of waste recycled</span>
+
+
+
+
+</div>
+<div class="whatsnew" style="font-family: 'Open Sans', sans-serif;font-weight:800;margin-top:35px;">
+<center>WHAT'S NEW</center>
+</div>
+<div class="row" style="margin-top:10px;margin-left:130px;font-family: 'Open Sans', sans-serif;
+font-weight:300">
+<div class="gallery">
+  <a target="_blank" href="http://www.keralacm.gov.in/2019/02/27/letter-prime-minister-trivandrum-airport/">
+    <img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>
+/images/cm.jpeg" alt="Cinque Terre"  style="width:350px;height:200px">
+  </a>
+  <div class="desc"><center>News</center><br>Chief Minister Meeting with Student Parliamentarians</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="http://localhost/ckcl/images/download.jpg">
+    <img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>
+/images/cm1.jpeg" style="width:350px;height:200px">
+  </a>
+  <div class="desc">CM Speaking at a function in New York</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="http://localhost/ckcl/images/download.jpg">
+    <img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>
+/images/cm3.jpeg"  style="width:350px;height:200px" >
+  </a>
+  <div class="desc">SingularityU India Global Impact Challenge participants Meet CM</div>
+</div></div>
+</body>
+
+
+</html>
