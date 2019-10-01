9b3be637dc4d16d49caf2da3fa2e00e46f1046ae
najlabathool <najla989j6@gmail.com>
2019-10-01 11:43:22 +0530

    fghhg

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <div class="container" style="background-image:url(templates/<?php echo $this->template ?>/images/download1.jpg)">
   <div class="menubar">
         <ul style="font-family: 'Open Sans', sans-serif;font-weight:800">
-        <li ><a href="">Clean Kerala <span>Company Ltd</span></a></li>
+        <li style="float:left" ><a href="">Clean Kerala <span>Company Ltd</span></a></li>
         <li style="float:right"><a href="">About</a></li>
         <li style="float:right"><a href="">Programmers</a></li>
         <li style="float:right"><a class="active1" href="#about">Contact</a></li>
@@ -29,69 +29,34 @@
 
 </div>
 
-<div class="secbar">
-    <ul style="list-style-type:none;padding:0px;margin-bottom:0px;margin-top:0px; background-color:#dcdee0;height:44px;font-family: 'Open Sans', sans-serif;
-font-weight:400">
-       <li><a style="display: block;float:left;
-        color: #093d17;width:682px;
-        text-align: left;
-        padding: 14px 16px;
-        padding-left:50px;
-        text-decoration:none;" href="">Clean Kerala<span>Company Ltd</span></a></li>
-      <li class="rightclass"style="float:left;"><a style="display: block;
-       color:white;
-       text-align: center;
-       padding: 14px 16px;
-      text-decoration: none;" href="" class="active">Home</a></li>
-      <li class="rightclass"><a  style="display: block;
-       color:#093d17;
-       text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;" href="">About us</a></li>
-        <li class="rightclass"><a  style="display: block;
-        color:#093d17;
-        text-align: center;
-         padding: 14px 16px;
-          text-decoration: none;" href="">Programmes</a></li>
-        <li class="rightclass"><a  style="display: block;
-  color:#093d17;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;" href="">Projects</a></li>
-  <li class="rightclass"><a style="display: block;
-  color:#093d17;
-  text-align:center;
-  padding: 14px 16px;
-  text-decoration:none" href="">Progress</a></li>
-  <li class="rightclass"><a  style="display: block;
-  color: #093d17;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;" href="">Media</a></li>
-  <li class="rightclass"><a  style="display: block;
-  color:#093d17;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration:none;" href="">Contact us</a></li>
-  </ul>
-</div>
+
+<ul style="background-color:grey;">
+  <li style="float:left"><a href="#home">Clean Kerala<span>Company Ltd</span></a></li>
+  <li style="float:right"><a href="#news">Contact us</a></li>
+
+  <li style="float:right"><a href="#news">Media</a></li>
+  <li style="float:right"><a href="#news">Projects</a></li>
+  <li style="float:right"><a href="#news">Programmes</a></li>
+  <li style="float:right"><a href="#contact">About us</a></li>
+  <li style="float:right""><a class="active" href="#about">Home</a></li>
+</ul>
+
+
 <div class="vd">
-  <center style="font-family: 'Open Sans', sans-serif;
-font-weight:800">  LOREM IPSUM DOLOR SIT AMET</center>
-<center style="font-family: 'Open Sans', sans-serif;
-font-weight:400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus mi sit amet libero elementum porta. Aliquam sit
+  <center style="font-family: 'Open Sans', sans-serif;font-weight:800">  LOREM IPSUM DOLOR SIT AMET</center>
+<center style="font-family: 'Open Sans', sans-serif;font-weight:400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus mi sit amet libero elementum porta. Aliquam sit
 <br>amet ultricies mi, ac faucibus velit. Morbi aliquet massa quam, ac hendrerit metus fermentum sed. Duis tempus dignissim</br>
 enim eu tempor. Aliquam elementum libero lectus, in condimentum diam venenatis nec. Donec eget dignissim elit.<br>
 Curabitur pulvinar felis a vulputate pretium. Sed id fringilla mi. Proin vel massa eros.</center>
-<center  style="padding-top: 25px;">
+<center style="padding-top: 25px">
 <iframe width="620" height="245" src="https://www.youtube.com/embed/tgbNymZ7vqY"></center>
 </iframe>
 </div>
-<div class="impact" >
+<div class="impact">
   <center style="font-family: 'Open Sans', sans-serif;
 font-weight:800">OUR IMPACT</center></div>
 
-<ul id="qwerty">
+<ul id="qwerty" style="margin-left:400px">
   <li>
     <ul>
       <li class="circ">1235</li>
@@ -101,9 +66,23 @@ font-weight:800">OUR IMPACT</center></div>
   <li>
     <ul>
       <li class="circ">1235</li>
-      <li class="cap">Kg of waste desposed.</li>
+      <li class="cap">Collection Units</li>
     </ul>
   </li>
+  <li>
+    <ul>
+      <li class="circ">1235</li>
+      <li class="cap">Local bodies</li>
+    </ul>
+  </li>
+  <li>
+    <ul>
+      <li class="circ">1235</li>
+      <li class="cap">Tones waste recycled.</li>
+    </ul>
+  </li>
+</ul>
+<ul id="qwerty" style="margin-left:400px">
   <li>
     <ul>
       <li class="circ">1235</li>
@@ -113,12 +92,24 @@ font-weight:800">OUR IMPACT</center></div>
   <li>
     <ul>
       <li class="circ">1235</li>
-      <li class="cap">Kg of waste desposed.</li>
+      <li class="cap">Collection Units</li>
+    </ul>
+  </li>
+  <li>
+    <ul>
+      <li class="circ">1235</li>
+      <li class="cap">Local bodies</li>
+    </ul>
+  </li>
+  <li>
+    <ul>
+      <li class="circ">1235</li>
+      <li class="cap">Tones waste recycled.</li>
     </ul>
   </li>
 </ul>
 
-</div>
+
 <div class="whatsnew" style="font-weight:800;margin-top:35px;">
 <center>WHAT'S NEW</center>
 </div>
@@ -133,7 +124,7 @@ font-weight:300">
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="http://localhost/ckcl/images/download.jpg">
+<a target="_blank" href="http://localhost/ckcl/images/download.jpg">
     <img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>
 /images/cm1.jpeg" style="width:350px;height:200px">
   </a>
