007d6ee1a07249c23c51f6c2a1750752e9c71a25
najlabathool <najla989j6@gmail.com>
2019-10-03 12:08:34 +0530

    hhh

--- footer.php
+++ footer.php
@@ -1,12 +1,18 @@
 <div class="footer" style="margin-top:350px;"> 
-    <div id="dlg">Keep the nature clean to save future</div>
+    <div style="color:white;"id="dlg">Keep the nature clean to save future</div>
      <div id="cnct" href=""> Stay connected with us <br>
+
      
 <ul class=" footercol footsocial">
 <a href="https://www.instagram.com/">
-<li><i class="fab fa-instagram"></i></li></a></ul>
+<li><i class="fab fa-instagram"></i></li></a>
+<a href="https://www.facebook.com/">
+<li><i class="fab fa-facebook"></i></li></a>
+<a href="https://www.gmail.com/">
+<li><i class="fab fa-gmail"></i></li></a>
+</ul>
   <div class="icons">   
-<ul  style="list-style-type:none;float:left">
+<ul  style="list-style-type:none;float:left;">
 <li><a href="">Home</a></li>
 <li ><a href="">About us</a></li>
 <li> <a href="">Projects</a></li>
@@ -16,7 +22,7 @@
 <li ><a href="">Contact us</a></li>
 <li><a href="">About</a></li>
 <li ><a href="">contact</a></li>
-<li>etc</li>
+<li><a href="">etc</a></li>
 </ul>
 <ul style="list-style-type:none;float:left">
 <li><a href="">Home</a></li>
